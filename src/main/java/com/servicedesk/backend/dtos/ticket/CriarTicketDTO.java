bff60ea727b9bfe6296c07fe67774449e283f935
Namibo06 <namibo654@gmail.com>
2025-08-28 19:13:09 -0300

    feat: criado dtos para criar, buscar e atualizar ticket

---
+++ src/main/java/com/servicedesk/backend/dtos/ticket/CriarTicketDTO.java
@@ -0,0 +1,12 @@
+package com.servicedesk.backend.dtos.ticket;
+
+import com.mongodb.lang.NonNull;
+import com.servicedesk.backend.dtos.MensagemStatusDTO;
+
+public record CriarTicketDTO(
+        @NonNull MensagemStatusDTO mensagemStatus,
+        @NonNull Long solicitanteUsuarioId,
+        @NonNull Long ticketAbertoUsuarioId,
+        @NonNull String descricaoProblema
+) {
+}
