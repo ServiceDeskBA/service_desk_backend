678e02a4e224decb49c7997b84cbd673e991dd88
Namibo06 <namibo654@gmail.com>
2025-08-28 19:12:28 -0300

    feat: criado dtos para criar, buscar e atualizar unidade

---
+++ src/main/java/com/servicedesk/backend/dtos/unidade/CriarUnidadeDTO.java
@@ -0,0 +1,10 @@
+package com.servicedesk.backend.dtos.unidade;
+
+import com.mongodb.lang.NonNull;
+import com.servicedesk.backend.dtos.MensagemStatusDTO;
+
+public record CriarUnidadeDTO(
+        @NonNull MensagemStatusDTO mensagemStatus,
+        @NonNull String nomeUnidade
+) {
+}
