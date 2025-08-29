34de0d1ebcfb7bd855b6f5a26d40303254d01fff
Namibo06 <namibo654@gmail.com>
2025-08-29 19:59:12 -0300

    refactor: correção nos retornos de todas interfaces

--- src/main/java/com/servicedesk/backend/interfaces/UsuarioInterface.java
+++ src/main/java/com/servicedesk/backend/interfaces/UsuarioInterface.java
@@ -1,6 +1,8 @@
 package com.servicedesk.backend.interfaces;
 
+import com.servicedesk.backend.dtos.MensagemStatusDTO;
 import com.servicedesk.backend.dtos.usuario.AtualizarUsuarioDTO;
+import com.servicedesk.backend.dtos.usuario.BuscarUsuarioDTO;
 import com.servicedesk.backend.dtos.usuario.CriarUsuarioDTO;
 import com.servicedesk.backend.entities.UnidadeEntity;
 import com.servicedesk.backend.entities.UsuarioEntity;
@@ -8,10 +10,10 @@ import com.servicedesk.backend.entities.UsuarioEntity;
 import java.util.List;
 
 public interface UsuarioInterface {
-    void criarUsuario(CriarUsuarioDTO criarUsuarioDados);
-    UsuarioEntity buscarUsuario(Long usuarioId);
-    List<UsuarioEntity> buscarTodosUsuarios();
-    void atualizarUsuario(Long usuarioId, AtualizarUsuarioDTO atualizarUsuarioDados);
+    UsuarioEntity criarUsuario(CriarUsuarioDTO criarUsuarioDados);
+    BuscarUsuarioDTO buscarUsuario(Long usuarioId);
+    List<BuscarUsuarioDTO> buscarTodosUsuarios();
+    MensagemStatusDTO atualizarUsuario(Long usuarioId, AtualizarUsuarioDTO atualizarUsuarioDados);
     void deletarUsuario(Long usuarioId);
     boolean verificaSeExisteUsuarioPorId(Long usuarioId);
 }
