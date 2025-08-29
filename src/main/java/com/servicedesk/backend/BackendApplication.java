556f3afd0eb09d202545825da5c6f72a18198f2d
Namibo06 <namibo654@gmail.com>
2025-08-29 19:58:32 -0300

    feat: adicionado nova anotação para habilitar a auditoria do Mongo

--- src/main/java/com/servicedesk/backend/BackendApplication.java
+++ src/main/java/com/servicedesk/backend/BackendApplication.java
@@ -2,7 +2,9 @@ package com.servicedesk.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
+@EnableMongoAuditing //permite que o Mongo possa setar coisas automáticas no banco, como createdAt
 @SpringBootApplication
 public class BackendApplication {
 	public static void main(String[] args) {
