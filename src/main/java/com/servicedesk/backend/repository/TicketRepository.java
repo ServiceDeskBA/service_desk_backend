79b621e721053420e766abc0abaaa7acdf71d3e3
Namibo06 <namibo654@gmail.com>
2025-08-29 21:46:08 -0300

    feat: criado repositorio para ticket

---
+++ src/main/java/com/servicedesk/backend/repository/TicketRepository.java
@@ -0,0 +1,7 @@
+package com.servicedesk.backend.repository;
+
+import com.servicedesk.backend.entities.TicketEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface TicketRepository extends MongoRepository<TicketEntity, Long> {
+}
