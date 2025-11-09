67112fc8c83a8e8b8b0c5184418464cd469f6684
Namibo06 <namibo654@gmail.com>
2025-11-08 23:54:13 -0300

    refactror: adjust variables and created files docker

--- src/main/java/com/servicedesk/backend/security/TokenService.java
+++ src/main/java/com/servicedesk/backend/security/TokenService.java
@@ -5,7 +5,6 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.servicedesk.backend.entities.UsuarioEntity;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -14,9 +13,7 @@ import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
-
-    @Value("${api.security.token.secret}")
-    private String secret;
+    private String secret = "serviceDeskProject6";
 
     private static final String ISSUER = "ServiceDesk API";
 
